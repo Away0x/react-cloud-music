ccc977d3a987d76352ab7fb96f3e5b6311e0116e
Away0x <wutong0910@foxmail.com>
2020-08-17 11:04:37 +0800

    feat: server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const compression = require('compression');
+
+const port = process.env.PORT || 8010;
+const app = express();
+
+app.use(compression());
+app.use(express.static('./build'));
+
+module.exports = app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(`Listening at http://localhost:${port}\n`);
+});
